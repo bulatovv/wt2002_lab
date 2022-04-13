d227876328abe6fc6f58b7bb64fee9da203e13ef
Albert <albert.fz@yandex.ru>
2022-04-13 12:34:41 +0500

    add type column for items table

--- database/migrations/2022_04_08_125007_create_items_table.php
+++ database/migrations/2022_04_08_125007_create_items_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('type');
             $table->string('description');
             $table->string('image');
             $table->timestamps();
