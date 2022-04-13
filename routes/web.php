9a196d43714a619d172ff5ea05ad6e49e2ae4c4a
Albert <albert.fz@yandex.ru>
2022-04-13 23:08:08 +0500

    add Item resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
-    return view('index');
+    return redirect()->route('items.index');
 });
+
+Route::resource('items', ItemController::class);
