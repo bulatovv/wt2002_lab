1c6abdead1a460d63647ff0d193517813d5ba4b6
Albert <albert.fz@yandex.ru>
2022-04-13 18:33:57 +0500

    add type field and mutators for Item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -11,7 +11,28 @@ class Item extends Model
 
     protected $fillable = [
         'name',
+        'type',
         'description',
         'image'
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+    }
+
+    public function setTypeAttribute($value)
+    {
+        $this->attributes['type'] = strtolower($value);
+    }
+
+    public function setDescriptionAttribute($value)
+    {
+        $this->attributes['description'] = preg_replace('/\s+/', ' ', trim($value));
+    }
+
+    public function setImageAttribute($value)
+    {
+        $this->attributes['image'] = $value;
+    }
 }
