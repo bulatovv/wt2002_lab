0e1532b7bc8281d41f021aa14c3dd8d3f71b4894
Albert <albert.fz@yandex.ru>
2022-04-16 04:45:34 +0500

    add html validation

--- resources/views/item_form.blade.php
+++ resources/views/item_form.blade.php
@@ -23,11 +23,12 @@
                 <label for="image">
                     @empty($item->image)
                         <img id="imagePreview" class="img-fluid rounded img-placeholder">
+                        <input id="image" type="file" name="image" style="display:none" required>
                     @else
                         <img id="imagePreview" class="img-fluid rounded" 
                              src="{{ asset('img/uploads/' . $item->image) }}">
+                        <input id="image" type="file" name="image" style="display:none">
                     @endif
-                    <input id="image" type="file" name="image" style="display:none">
                 </label>
                 <script>
                     image.onchange = e => {
@@ -43,15 +44,16 @@
             <div class="right w-50">
                 <div class="form-group mb-2">
                     <label for="name">Название объекта</label>
-                    <input id="name" name="name" class="form-control" value="{{ $item->name ?? ''}}"></input>
+                    <input id="name" name="name" class="form-control" value="{{ $item->name ?? ''}}" required>
                 </div>
                 <div class="form-group mb-2">
                     <label for="type">Тип</label>
-                    <input id="type" name="type" class="form-control" value="{{ $item->type ?? ''}}"></input>
+                    <input id="type" name="type" class="form-control" value="{{ $item->type ?? ''}}" required>
                 </div>
                 <div class="form-group mb-2">
                     <label for="description">Описание</label>
-                    <input id="description" name="description" class="form-control" value="{{ $item->description ?? ''}}"></input>
+                    <input id="description" name="description" class="form-control"
+                           value="{{ $item->description ?? ''}}" required>
                 </div>
                 <button type="submit" class="btn btn-success">Добавить</button>
             </div>
