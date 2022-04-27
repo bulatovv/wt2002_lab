7d49f26eeff2ffec8cb066053349c0fa4a09209c
Albert <albert.fz@yandex.ru>
2022-04-28 01:41:44 +0500

    add breeze to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,11 @@ window.jQuery = $;
 window.$ = $;
 window.bootstrap = bootstrap;
 
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+
+
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
@@ -31,4 +36,3 @@ $(document).keydown(function(e){
         break;
     }
 });
-
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,20 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css')
-    .copy('resources/img/*.svg', 'public/img/')
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
+
+
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+]);
